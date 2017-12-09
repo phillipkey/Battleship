8495b8fa862c770451779c8854eae647174db404
Phillip Key <key.phillip@gmail.com>
2017-12-09 13:54:48 -0500

    add shipId to cell for lookup after hit

---
+++ src/app/cell.ts
@@ -0,0 +1,14 @@
+export class Cell {
+    value: string;
+    isChecked: boolean;
+    xPos: number;
+    yPos; number;
+    shipId: number;
+    constructor(x: number, y: number, value: string) {
+        this.xPos = x;
+        this.yPos = y;
+        this.value = value;
+        this.isChecked = false;
+        this.shipId = -1;
+    }
+}
