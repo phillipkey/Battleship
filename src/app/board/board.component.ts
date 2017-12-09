4c0dcc25a667234a5379e805096149d8269763e3
Phillip Key <key.phillip@gmail.com>
2017-12-09 13:59:51 -0500

    import newly created ship, cell, ship service classes to board component; implement functions to handle user clicks on the board as well as check if a ship was hit, whether it was sunk, and whether the game is over.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Ship } from '../ship';
+import { Cell } from '../cell';
+import { ShipService } from '../ship.service';
+import { isDefaultChangeDetectionStrategy } from '@angular/core/src/change_detection/constants';
 
 @Component({
   selector: 'app-board',
@@ -6,27 +10,57 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  gameboard = [
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0],
-                [0,0,0,0,0,0,0,0,0,0]
-  ];
-  constructor() { }
+  cols = 10;
+  rows = 10;
+  gameboard: Cell[] = [];
+  ships: Ship[];
+
+  constructor(private shipService: ShipService) { }
 
   ngOnInit() {
+   this.shipService.getShipset1().subscribe(ships => this.ships = ships);
+   var player1Selector = Math.floor(Math.random()*(4-1+1)+1);
+   alert(player1Selector);
+    //build board
+    var temp: Cell[];
+    for (var i = 1; i <= this.rows; i++){
+      for (var j = 1; j <= this.cols; j++) {
+        this.gameboard.push(new Cell(i,j,"O"));
+      }
+    }
     //randomly place a ship.
     //will need to make sure ship has space.
     //"ship" component needs to be created and imported here
   }
 
-  onClick() {
-    alert("Evaluate hit or miss!");
+  onClick(cell: Cell): void {
+    if (cell.isChecked) {
+      alert("Already Checked!");
+    } else {
+      cell.isChecked = true;
+      if (this.isHit(cell)) {
+        //check if ship is sunk
+      }
+      alert(cell.xPos + ", " + cell.yPos);
+    }
+  }
+
+  private isHit(cell: Cell): boolean {
+    if (cell.value != "O") {
+      var ship = this.ships.find(ship => ship.id === cell.shipId);
+      ship.hit();
+      if (ship.isSunk)
+         this.ships.splice(this.ships.indexOf(ship), 1);
+      if (this.isGameOver()) {
+        //gameover
+      }
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  private isGameOver(): boolean {
+    return this.ships.length === 0;
   }
 }
