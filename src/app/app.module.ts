4f477cd490bbef830ae4704f9388ff77491a5344
Phillip Key <key.phillip@gmail.com>
2017-12-08 23:23:38 -0500

    create components for ships and players

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BoardComponent } from './board/board.component';
+import { ShipComponent } from './ship/ship.component';
+import { PlayerComponent } from './player/player.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardComponent
+    BoardComponent,
+    ShipComponent,
+    PlayerComponent
   ],
   imports: [
     BrowserModule
