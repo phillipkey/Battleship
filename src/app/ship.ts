7981e33e4e2f4e76514b2e1cf3bed1ae33858ea9
Phillip Key <key.phillip@gmail.com>
2017-12-10 22:38:25 -0500

    add a readonly property to safely read the length of a given ship.

--- src/app/ship.ts
+++ src/app/ship.ts
@@ -2,18 +2,21 @@ export class Ship {
     id: number;
     name: string;
     health: number;
-    symbol: string; //this will be an array of points [[x1,y1], ... , [xn, yn]
-    position = [];
+    symbol: string; 
+    position = []; //this will be an array of points [[x1,y1], ... , [xn, yn]
+    readonly totalHealth; //used for measurements, so make a read only property    
 
     constructor(id: number, name: string, health: number, symbol: string, position: any[]){
         this.id = id;
         this.name = name;
         this.health = health;
+        this.totalHealth = health;
         this.symbol = symbol;
         this.position = position;
     }
 
     hit(): void {
+        console.log(this.name + ' has been hit');
         this.health--;
     }
 
