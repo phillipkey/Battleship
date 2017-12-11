304770f7542399084933e7c4682334a0ce08af90
Phillip Key <key.phillip@gmail.com>
2017-12-10 22:40:17 -0500

    relegate some dependencies to the game service.

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Ship } from '../ship';
 import { Cell } from '../cell';
+import { PlayerGrid } from '../playerGrid'
+import { SHIPSET1 } from '../mock-ships';
 import { ShipService } from '../ship.service';
 import { MessageService } from '../message.service';
 import { isDefaultChangeDetectionStrategy } from '@angular/core/src/change_detection/constants';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-board',
@@ -11,70 +14,80 @@ import { isDefaultChangeDetectionStrategy } from '@angular/core/src/change_detec
   styleUrls: ['./board.component.css']
 })
 export class BoardComponent implements OnInit {
-  currentPlayer = true; //true = player 1, false = player 2
-  cols = 10;
-  rows = 10;
-  gameboard: Cell[] = [];
-  ships: Ship[];
+  player1Name = "Player 1";
+  player2Name = "Player 2";
 
-  constructor(private shipService: ShipService, private messageService: MessageService) { }
+  constructor(private shipService: ShipService, private messageService: MessageService, private gameService: GameService) { }
 
   ngOnInit() {
-   this.shipService.getShipset1().subscribe(ships => this.ships = ships);
-   //random selector for different shipset to be implemented later.
-   var player1Selector = Math.floor(Math.random()*(4-1+1)+1);
-    //build board
-    var temp: Cell[];
-    for (var i = 1; i <= this.rows; i++){
-      for (var j = 1; j <= this.cols; j++) {
-        this.gameboard.push(new Cell(i,j,"O"));
-      }
-    }      
-    this.ships.forEach(ship => {
-      ship.position.forEach(point => {
-        //alert(point[0]);
-        //alert(point[1]);
-        var shipCell = this.gameboard.find(gameboardCell => gameboardCell.xPos == point[0] 
-          && gameboardCell.yPos == point[1]);
-          shipCell.value = ship.symbol;
-          shipCell.shipId = ship.id;
-      });
-    });
-    //for each ship, update corresponding cell with shipId and value.
+    console.log("Setup: " + this.gameService.isSetup);
   }
 
-  onClick(cell: Cell): void {
-    if (cell.isChecked) {
-      this.messageService.add("This cell has already been checked!")
+  placeShip(cell:Cell): void {
+    var ship = this.gameService.currentShipToPlace;
+    console.log("attempting to place ship");
+    if (cell.value != "O") {
+      this.gameService.turnMessage = "There is already a ship here. Please select a valid location.";
+      return;
+    }
+    console.log('place a ship');
+    if (this.gameService.startPlacement && cell.validStartPoint(ship)) {
+      ship.position.push([cell.xPos, cell.yPos]);
+      cell.highlight;
+      this.gameService.startPlacement = false;
+      this.gameService.turnMessage = "Select the end point for your " + ship.name;
     } else {
-      this.currentPlayer = !this.currentPlayer;
-      cell.isChecked = true;
-      if (!this.isHit(cell)) {
-        this.messageService.add("*SPLASH* Miss!")
+      if (cell.validEndPoint(ship)) {
+        if (this.gameService.placeShip(ship)) {
+          ship.position.push([cell.xPos, cell.yPos]);
+          this.gameService.turnMessage = "SHIP PLACED!"
+        } else {
+          this.gameService.turnMessage = "Obstruction!"
+        }
+      } else {
+        this.gameService.turnMessage = "Not a valid ship placement"
       }
     }
   }
 
-  private isHit(cell: Cell): boolean {
+  playerFire(cell: Cell): void {
+    console.log('cell was clicked');
+    if (cell.isChecked) {
+      this.messageService.add("This cell has already been checked!")
+    } else {
+      cell.isChecked = true;
+      if (this.gameService.turn == 0) {
+        if (!this.isHit(cell, this.gameService.player1Ships)) {
+          this.messageService.add("*SPLASH* Miss!")
+        }
+      } else {
+        if (!this.isHit(cell, this.gameService.player2Ships)) {
+          this.messageService.add("*SPLASH* Miss!")
+        }
+      }
+      this.gameService.changeTurn();
+    }
+  }
+
+  private isHit(cell: Cell, ships: Ship[]): boolean {
+    console.log('checking for a hit...');
     if (cell.value != "O") {
-      var ship = this.ships.find(ship => ship.id === cell.shipId);
+      var ship = ships.find(ship => ship.id === cell.shipId);
       ship.hit();
       if (ship.isSunk()) {
+        console.log('ship sunk');
         this.messageService.add("You destroyed their " + ship.name + "!");
-        this.ships.splice(this.ships.indexOf(ship), 1);
+        ships.splice(ships.indexOf(ship), 1);
       } else {
         this.messageService.add("*BOOM* You hit something!")
       }
-      if (this.isGameOver()) {
+      if (this.gameService.isGameOver()) {
+        console.log('game over, reset board');
         this.messageService.add("YOU WIN!");
       }
       return true;
     } else {
       return false;
     }
-  }
-
-  private isGameOver(): boolean {
-    return this.ships.length === 0;
   }
 }
